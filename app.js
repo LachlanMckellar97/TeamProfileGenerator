d1c5e1c884ee8fedaa353232d8fba07d2d0c9f44
Lachlan Mckellar <lachlan.person@gmail.com>
2020-05-04 22:24:50 +1000

    created classes and updated app.js

--- app.js
+++ app.js
@@ -12,39 +12,57 @@ const render = require("./lib/htmlRenderer");
 
 // Initial Questions
 
-const initialQuestions = [
-    {
-        type: "input",
-        name: "name",
-        message: "What is the employee's name?"
-    },
-    {
-        type: "input",
-        name: "id",
-        message: "What is the employee's ID?"
-    },
-    {
-        type: "input",
-        name: "email",
-        message: "What is the employee's email address?"
-    },
-    {
-        type: "list",
-        name: "role",
-        message: "What position applies to the employee?",
-        choices: [
-            "manager",
-            "engineer",
-            "intern"
-        ]
-    }
-]
+function Questions() {
 
-function init() {
-    inquirer.prompt(initialQuestions)
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "name",
+            message: "What is the employee's name?"
+        },
+        {
+            type: "input",
+            name: "id",
+            message: "What is the employee's ID?"
+        },
+        {
+            type: "input",
+            name: "email",
+            message: "What is the employee's email address?"
+        },
+        {
+            type: "list",
+            name: "role",
+            message: "What position applies to the employee?",
+            choices: [
+                "manager",
+                "engineer",
+                "intern"
+            ]
+        }
+    ])
+    .then(function(response) {
+        switch(response.role) {
+            case "manager":
+
+                inquirer.prompt([
+                    {
+                        type: "input",
+                        name: "officeNumber",
+                        message: "Please enter your manager's office number."
+                        
+                    }
+                ])
+        }
+    })
+
 }
 
+function init() {
+    Questions()
+}
 init()
+
 // Write code to use inquirer to gather information about the development team members,
 // and to create objects for each team member (using the correct classes as blueprints!)
 
