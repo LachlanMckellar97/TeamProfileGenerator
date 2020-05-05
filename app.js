f17131583303812cb1cf9060d32773291d860e7c
Lachlan Mckellar <lachlan.person@gmail.com>
2020-05-05 12:49:59 +1000

    adding jest and testing

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-const Manager = require("./lib/Manager");
-const Engineer = require("./lib/Engineer");
-const Intern = require("./lib/Intern");
+const RoleType = {
+    Manager: require("./lib/Manager"),
+    Engineer: require("./lib/Engineer"),
+    Intern: require("./lib/Intern")
+}
 const inquirer = require("inquirer");
 const path = require("path");
 const fs = require("fs");
@@ -9,8 +11,9 @@ const OUTPUT_DIR = path.resolve(__dirname, "output");
 const outputPath = path.join(OUTPUT_DIR, "team.html");
 
 const render = require("./lib/htmlRenderer");
+var employees = []
 
-// Initial Questions
+// Initial questions
 
 function Questions() {
 
@@ -35,53 +38,62 @@ function Questions() {
             name: "role",
             message: "What position applies to the employee?",
             choices: [
-                "manager",
-                "engineer",
-                "intern"
+                "Manager",
+                "Engineer",
+                "Intern"
             ]
         }
-    ])
-    .then(function(response) {
-        switch(response.role) {
-            case "manager":
+    ]).then(function(answer) {
+        SecondQuestions(answer)
+    })
+}
+//Call initial questions
+Questions()
 
-                inquirer.prompt([
-                    {
-                        type: "input",
-                        name: "officeNumber",
-                        message: "Please enter your manager's office number."
-                        
-                    }
-                ])
+// Role related questions
+function SecondQuestions({name, id, email, role}) {
+    if (role === "Manager") {
+        var variableTrait = "the managers Office number"
+    } else if (role === "Engineer") {
+        var variableTrait = "the engineers GitHub username"
+    } else {
+        var variableTrait = "the interns school"
+    }
+
+    inquirer.prompt([
+        {
+            type: 'input',
+            name: 'answer',
+            message: `What is ${variableTrait}?`
+        }
+    ]).then(function({answer}) {
+        employees.push(new RoleType[role](name, id, email, answer))
+        FinalQuestion()
+    })
+}
+
+// Add more employee's or finalise
+function FinalQuestion() {
+    inquirer.prompt([
+        {
+            type: "list",
+            name: "ans",
+            message: "Would you like to add another employee, if not, select no and the page will be created.",
+            choices: [
+                "Yes",
+                "No"
+            ]
+        }
+    ]).then(function({ans}) {
+        if (ans === "Yes") {
+            console.log("Add another employee")
+            Questions()
+        } else {
+            console.log("Successfully written to main.html")
+            var html = render(employees)
+            fs.writeFile(outputPath, html, err => {
+                if (err) throw err
+            })
         }
     })
-
 }
-
-function init() {
-    Questions()
-}
-init()
-
-// Write code to use inquirer to gather information about the development team members,
-// and to create objects for each team member (using the correct classes as blueprints!)
-
-// After the user has input all employees desired, call the `render` function (required
-// above) and pass in an array containing all employee objects; the `render` function will
-// generate and return a block of HTML including templated divs for each employee!
-
-// After you have your html, you're now ready to create an HTML file using the HTML
-// returned from the `render` function. Now write it to a file named `team.html` in the
-// `output` folder. You can use the variable `outputPath` above target this location.
-// Hint: you may need to check if the `output` folder exists and create it if it
-// does not.
-
-// HINT: each employee type (manager, engineer, or intern) has slightly different
-// information; write your code to ask different questions via inquirer depending on
-// employee type.
-
-// HINT: make sure to build out your classes first! Remember that your Manager, Engineer,
-// and Intern classes should all extend from a class named Employee; see the directions
-// for further information. Be sure to test out each class and verify it generates an
-// object with the correct structure and methods. This structure will be crucial in order
-// for the provided `render` function to work! ```
